f52e49dfc858abf75b8cfed481b6c9fbff1e6089
bruuhyan <163632842+bruuhyan@users.noreply.github.com>
2024-10-25 11:35:55 +0700

    tes

--- lab/register.php
+++ lab/register.php
@@ -39,13 +39,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $mail->setFrom("noreply@example.com");
             $mail->addAddress($email);
             $mail->Subject = "Account Activation";
-            $mail->Body = <<<END
-            Click <a href="https://darkslategray-woodpecker-113089.hostingersite.com/activate-account.php?token=$activation_token">here</a> to activate your account.
-            END;
-
             // $mail->Body = <<<END
-            // Click <a href="http://localhost/webprog/webprog-lab/lab/activate-account.php?token=$activation_token">here</a> to activate your account.
+            // Click <a href="https://darkslategray-woodpecker-113089.hostingersite.com/activate-account.php?token=$activation_token">here</a> to activate your account.
             // END;
+
+            $mail->Body = <<<END
+            Click <a href="http://localhost/webprog/webprog-lab/lab/activate-account.php?token=$activation_token">here</a> to activate your account.
+            END;
 
             try {
                 $mail->send();
